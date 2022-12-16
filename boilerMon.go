1259d355c4d7d3e30cb8024efe3e8658ee822605
dugwill <douglaswi1@comcast.net>
2022-12-16 12:57:25 -0700

    working with real boiler

--- boilerMon.go
+++ boilerMon.go
@@ -11,15 +11,19 @@ var err error
 
 func main() {
 
-	if boiler, err = triangleTube.NewBoiler(1); err != nil {
+	if boiler, err = triangleTube.NewBoiler(1, "com3"); err != nil {
 		fmt.Printf("Error creating new boiler instance: %v", err)
 		return
 	}
+
+	//fmt.Println("Slave ID: ", boiler.ReportSlaveID())
 
 	if err = boiler.Update(); err != nil {
 		fmt.Printf("Error updating boiler: %v\n", err)
 	} else {
 		fmt.Printf("Boiler Temp: %v\n", boiler.BoilerSupplyTemp)
+		fmt.Printf("Boiler Return Temp: %v\n", boiler.BoilerReturnTemp)
+
 	}
 
 }
